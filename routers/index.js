3beeec6006feb6c39c84751ef9cb08cf1a398d0c
Nguyễn Phước Công <88037699+NPC2704@users.noreply.github.com>
2023-08-14 16:24:44 +0700

    update DB

--- routers/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { tasksRouters } = require("./tasks.routers");
-const rootRouter = express.Router();
-
-// rootRouter.use("/tasks", tasksRouters);
-// module.exports = {
-//   rootRouter,
-// };
