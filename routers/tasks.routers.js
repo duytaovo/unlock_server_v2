a93b0d972148cf1a5064da3e07f2e32b467dfbcf
Nam <realcm471@gmail.com>
2023-08-14 11:40:25 +0700

    .

--- routers/tasks.routers.js
+++ routers/tasks.routers.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const {
-  createTask,
-  getAllTask,
-  deleteTask,
-  updateTask,
-} = require("../controllers/task.controllers");
-const tasksRouters = express.Router();
+// const express = require("express");
+// const {
+//   createTask,
+//   getAllTask,
+//   deleteTask,
+//   updateTask,
+// } = require("../controllers/task.controllers");
+// const tasksRouters = express.Router();
 
-tasksRouters.post("/", createTask);
-tasksRouters.get("/", getAllTask);
-tasksRouters.delete("/:id", deleteTask);
-tasksRouters.put("/:id", updateTask);
-module.exports = {
-  tasksRouters,
-};
+// tasksRouters.post("/", createTask);
+// tasksRouters.get("/", getAllTask);
+// tasksRouters.delete("/:id", deleteTask);
+// tasksRouters.put("/:id", updateTask);
+// module.exports = {
+//   tasksRouters,
+// };
